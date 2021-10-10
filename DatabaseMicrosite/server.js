067619d119b40d1aa574252398f3afd2c72bb723
Melson <melsonzpcanlas@gmail.com>
2021-10-11 03:29:25 +0800

    Added programdata route in server

--- DatabaseMicrosite/server.js
+++ DatabaseMicrosite/server.js
@@ -7,12 +7,14 @@ const port = process.env.PORT || 3000;
 
 const UserRouter = require('./api/User');
 const AnnouncementRouter = require('./api/AnnouncementData');
+const ProgramRouter = require('./api/ProgramData');
 
 const bodyParser = require('express').json;
 app.use(bodyParser());
 
 app.use('/user', UserRouter);
 app.use('/announcementdata', AnnouncementRouter);
+app.use('/programdata', ProgramRouter);
 
 app.listen(port, () => {
     console.log('The server is connected on port ' + port);
