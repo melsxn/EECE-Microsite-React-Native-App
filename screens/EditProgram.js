0d8c565c261495a824b769a3cd7401ebe006cb14
Melson <melsonzpcanlas@gmail.com>
2021-10-13 02:49:13 +0800

    Add Add/Edit/Delete Program UI (Han)

--- screens/EditProgram.js
+++ screens/EditProgram.js
@@ -1,25 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-
-
-
-export default function EditProgram() {
-  return (
-    <SafeAreaView style={styles.container}>
-            <Text>Edit Program! UI Temporary for navigation testing</Text>
-            <StatusBar style="light" />
-
-    </SafeAreaView>
-  );
-}
+import * as React from 'react';
+import { useState } from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {
+  View,
+  Text,
+  Button,
+  Image,
+  StyleSheet,
+  TextInput,
+  Picker,
+} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
+  textInput: {
     flex: 1,
-    backgroundColor: "dodgerblue",
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingLeft: 10,
+    color: '#05375a',
   },
   logo: {
     width: "100%",
@@ -27,4 +24,53 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: -200,
   }
-});
+})
+
+function EditProgramScreen ({ navigation }) {
+  const [selectedValue, setSelectedValue] = useState("java");
+  return (
+    <View style={{ flex: 1, alignContent: 'center',  marginTop: 200, marginBottom: 200 }}>
+      <Image
+        style={styles.logo}
+        source={require('../assets/EECE-Banner.jpg')}
+      />
+      <Picker
+        selectedValue={selectedValue}
+        style={{ height: 30, width: 200 }}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Bachelor's Degrees" value="BS" />
+        <Picker.Item label="Master's Degrees" value="MS" />
+        <Picker.Item label="PhD Programs" value="PP" />
+      </Picker>
+      <View style={{ flex: 1, }}>
+        <TextInput
+          placeholder="Program Name"
+          placeholderTextColor="#666666"
+          style={styles.textInput}
+        />
+      </View>
+      <View style={{ flex: 2, }}>
+        <TextInput
+          multiline
+          numberOfLines={10}
+          placeholder="Description"
+          placeholderTextColor="#666666"
+          style={styles.textInput}
+        />
+      </View>
+        <Button
+          title="Update"
+          onPress={() => navigation.navigate("EditProgram")}
+          color="#BD2B0C"
+        />
+        <Button
+          title="Back"
+          onPress={() => navigation.navigate("AdminScreen")}
+          color="#BD2B0C"
+        />
+      </View>
+  )
+}
+
+export default EditProgramScreen
