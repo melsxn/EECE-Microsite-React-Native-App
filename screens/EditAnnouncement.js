18ab1b46b16beac629a89a0a327a91d3e0cede3b
Melson <melsonzpcanlas@gmail.com>
2021-10-15 03:17:14 +0800

    Add connection between AnnouncementData api and EditAnnouncement UI

--- screens/EditAnnouncement.js
+++ screens/EditAnnouncement.js
@@ -1,34 +1,78 @@
 import React from "react";
 import { View, StyleSheet, TextInput, Text, Button, Alert } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { Formik } from "formik";
+
+import axios from "axios";
 
 const EditAnnounce = () => {
+  const handleEdit = (credentials)=> {
+    const uri = 'http://192.168.1.10:3000/announcementdata/editannouncement';
+    axios.post(uri, credentials).then((response) => {
+      const results = response.data;
+      const {message, status, data} = results;
+      console.log(status);
+      
+      // if condition here for update output
+      if(status !== 'SUCCESS'){
+        console.log("Not Success");
+      } else {
+        console.log("Description successfully updated!");
+      }
+        
+    })
+    .catch(error => {
+        console.log("error");
+    })
+}
+
   return (
-    <View>
-    <View style={styles.header}>
-      <Icon name='arrow-left' size={20} style={styles.icon}/>
-      <Text style={styles.screenName}>Edit Announcements</Text>
-    </View>
-    <View style={styles.search}>
-      <TextInput  
-      placeholder={'   Title of Announcement'}
-      style={styles.searchBox}
-      required={true}
-      />
-      <TextInput  
-      placeholder={'   Date'}
-      style={styles.searchBox}/>
-      <TextInput 
-      multiline
-      placeholder={'   Description'}
-      style={styles.searchBox}
-      required={true}/>
-       <Button
-        title="Update/Edit"
-        color='#000000'
-      />
-    </View>
-    </View>
+    <Formik
+    initialValues={{ Title:'', Date:'', Description:'' }}
+    onSubmit={(values) => {
+        console.log(values);
+  
+        handleEdit(values);
+        
+    }}
+  >{({handleChange,handleBlur,handleSubmit, values}) => (
+        <View>
+        <View style={styles.header}>
+          <Icon name='arrow-left' size={20} style={styles.icon}/>
+          <Text style={styles.screenName}>Edit Announcements</Text>
+        </View>
+        <View style={styles.search}>
+          <TextInput  
+          placeholder={'   Title of Announcement'}
+          style={styles.searchBox}
+          onChangeText={handleChange('Title')}
+          values={values.Title}
+          required={true}
+          />
+          <TextInput  
+          placeholder={'   Date'}
+          style={styles.searchBox}
+          onChangeText={handleChange('Date')}
+          values={values.Date}
+          />
+          <TextInput 
+          multiline
+          placeholder={'   Description'}
+          style={styles.searchBox}
+          onChangeText={handleChange('Description')}
+          values={values.Description}
+          required={true}
+          />
+          <Button
+            title="Update/Edit"
+            color='#000000'
+            onPress={handleSubmit}
+          />
+        </View>
+        </View>
+  )}
+  
+  </Formik>
   );
 }
 
