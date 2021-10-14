d2177372c36a18e6f77a076ca07514def4f133a4
Melson <melsonzpcanlas@gmail.com>
2021-10-15 02:35:21 +0800

    Add connection between AnnouncemantData api and AddAnnoucement UI

--- screens/AddAnnoucement.js
+++ screens/AddAnnoucement.js
@@ -2,8 +2,45 @@ import React from "react";
 import { View, StyleSheet, TextInput, Text, Button, Alert } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import { Formik } from "formik";
+
+import axios from "axios";
+
 const AddAnnounce = () => {
+  const handleAdd = (credentials)=> {
+    const uri = 'http://192.168.1.10:3000/announcementdata/addannouncement';
+    //http://localhost:3000/announcementdata/editannouncement
+    axios.post(uri, credentials).then((response) => {
+      const results = response.data;
+      const {message, status, data} = results;
+      console.log(status);
+      
+      // if condition here to direct to admin ui 
+      if(status !== 'SUCCESS'){
+        console.log("Not Success");
+      } else {
+        console.log("Announcement Successfully added!!");
+      }
+        
+    })
+    .catch(error => {
+        console.log("error");
+    })
+}
+const handleMessage = (message, type = 'FAILED') => {
+  setMessage(message);
+  setMessageType(type);
+}
   return (
+<Formik
+    initialValues={{ Title:'', Date:'', Description:'' }}
+    onSubmit={(values) => {
+        console.log(values);
+  
+        handleAdd(values);
+        
+    }}
+  >{({handleChange,handleBlur,handleSubmit, values}) => (
     <View>
     <View style={styles.header}>
       <Icon name='arrow-left' size={20} style={styles.icon}/>
@@ -13,22 +50,33 @@ const AddAnnounce = () => {
       <TextInput  
       placeholder={'   Title of Announcement'}
       style={styles.searchBox}
+      onChangeText={handleChange('Title')}
+      values={values.Title}
       required={true}
       />
       <TextInput  
       placeholder={'   Date'}
-      style={styles.searchBox}/>
+      style={styles.searchBox}
+      onChangeText={handleChange('Date')}
+      values={values.Date}
+      />
       <TextInput 
       multiline
       placeholder={'   Description'}
       style={styles.searchBox}
-      required={true}/>
+      required={true}
+      onChangeText={handleChange('Description')}
+      values={values.Description}
+      />
        <Button
         title="Submit"
         color='#000000'
+        onPress={handleSubmit}
       />
     </View>
     </View>
+  )}
+  </Formik>
   );
 }
 
