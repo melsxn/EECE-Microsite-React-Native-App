4e984af21cedff632ef56c11e0924e977752169b
Melson <melsonzpcanlas@gmail.com>
2021-10-17 03:01:54 +0800

    Add connection between ProgramData api and DeleteProgram UI

--- screens/DeleteProgram.js
+++ screens/DeleteProgram.js
@@ -11,25 +11,104 @@ import {
   TextInput,
   Picker,
 } from 'react-native';
+import { Formik } from 'formik';
+import axios from 'axios';
 
 const styles = StyleSheet.create({
   textInput: {
-    flex: 1,
-    paddingLeft: 10,
     color: '#05375a',
+    borderWidth: 1,
+    borderRadius:6,
+    marginBottom:10,
+    width:350,
+
   },
   logo: {
     width: "100%",
     height: 120,
     position: 'absolute',
     top: -200,
+  },
+  header:{
+    marginBottom:20,
+  },
+  screenName:{
+    alignSelf:'center',
+    fontWeight:'bold',
+    fontSize:20,
+    color:'#FFFFFF',
+    marginBottom:10
+  },
+  icon:{
+    marginTop:40,
+    marginLeft:15
+  },
+  search:{
+    alignItems:'center',
+    flexDirection:'column',
+    marginTop:20,
+    paddingLeft:10
+  },
+  searchBox:{
+    height:35,
+    width:250,
+    borderWidth:1,
+    borderRadius:6,
+    marginBottom:10
+  },
+  searchBox2:{
+    height:35,
+    width:250,
+    borderWidth:1,
+    borderRadius:6,
+    marginBottom:10,
+    maxHeight:80,
+    minHeight:10,
+    flex: 5
+  },
+  space:{
+    height:10,
+    width:10
   }
 })
 
-function DeleteProgramScreen ({ navigation }) {
+const DeleteProgramScreen = ({navigation}) =>  {
   const [selectedValue, setSelectedValue] = useState("java");
+  const handleDelte = (credentials)=> {
+    const uri = 'http://192.168.1.10:3000/programdata/deleteprogram';
+    axios.post(uri, credentials).then((response) => {
+      const results = response.data;
+      const {message, status, data} = results;
+      console.log(status);
+      
+      // if condition here of delete program
+      if(status !== 'SUCCESS'){
+        console.log("Not Success");
+      } else {
+        console.log("Program Successfully Delete!!");
+      }
+        
+    })
+    .catch(error => {
+        console.log("error");
+    })
+  }
+
+  const handleMessage = (message, type = 'FAILED') => {
+    setMessage(message);
+    setMessageType(type);
+  }
   return (
-    <View style={{ flex: 1, alignContent: 'center',  marginTop: 200, marginBottom: 200 }}>
+    <Formik
+    initialValues={{ TitleProgram:''}}
+    onSubmit={(values) => {
+        console.log(values);
+  
+        handleDelte(values);
+        
+    }}
+  >{({handleChange,handleBlur,handleSubmit, values}) => (
+    <View style={{ flex: 1, alignContent: 'center',  marginTop: 200, marginBottom: 100 }}>
       <Image
         style={styles.logo}
         source={require('../assets/EECE-Banner.jpg')}
@@ -43,24 +122,34 @@ function DeleteProgramScreen ({ navigation }) {
         <Picker.Item label="Master's Degrees" value="MS" />
         <Picker.Item label="PhD Programs" value="PP" />
       </Picker>
-      <View style={{ flex: 1, }}>
+      <View style={styles.search}>
         <TextInput
-          placeholder="Program Name"
+          placeholder="    Program Name"
           placeholderTextColor="#666666"
-          style={styles.textInput}
+          onChangeText={handleChange('TitleProgram')}
+          values={values.TitleProgram}
+          style={styles.searchBox}
         />
       </View>
-        <Button
-          title="Delete"
 
-          color="#BD2B0C"
+      <View style={{padding:10}}  >
+       <Button 
+          title="Add"
+          color="#000000"
+          onPress={handleSubmit}
         />
+        <View style={styles.space} />
         <Button
           title="Back"
-          color="#BD2B0C"
           onPress={() => navigation.navigate("AdminScreen")}
+          color="#000000"
         />
+
       </View>
+
+      </View>
+  )}
+  </Formik>
   )
 }
 
