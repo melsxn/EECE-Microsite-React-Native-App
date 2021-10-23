21d81b9b960c1e61ca69de447a4d7a035ee61f02
Melson <melsonzpcanlas@gmail.com>
2021-10-23 22:06:49 +0800

    Add three Program Screen (Bachelor, Master, PhD)

---
+++ screens/MasterScreen.js
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, TextInput, Text, Button, Alert, Image, ScrollView } from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Card } from 'react-native-elements'
+import axios from "axios";
+
+
+
+function MasterScreen() {
+
+
+  const[result, setResult] = useState([]);
+
+
+  useEffect(() => {
+      handleEdit()
+  }, [])
+
+  const handleEdit = ()=> {
+    const uri = 'http://192.168.1.11:3000/programdata/getmaster';
+    
+    const credentials = '';
+    axios.post(uri, credentials).then((response) => {
+      const results = response.data;
+     const {status, Date, data} = results;
+
+      setResult(data)
+
+
+    })
+    .catch(error => {
+        console.log("error");
+    })
+  }
+  return (
+    <ScrollView>
+    <View>
+    <View style={styles.header}>
+      <Icon name='arrow-left' size={20} style={styles.icon}/>
+      <Text style={styles.screenName}>Bachelor's Degrees</Text>
+    </View>
+    <View>
+      <Image source={require('../assets/EECE-Banner.jpg')} style={styles.banner} resizeMode="contain"/>
+
+    </View>
+    <DisplayData props = {result} />
+    </View>
+    </ScrollView>
+
+
+
+
+  );
+}
+
+const DisplayData = ({props}) => {
+    
+  const {status, message, data} = props;
+  //console.log(props[2].Date)
+    if (props.length > 0) {
+      return(
+        props.map((prop, index) => {
+          
+          return (
+            <Card key ={index}>
+                <Text style={styles.title}>{prop.TitleProgram}</Text>
+                <Text style={styles.date}>{prop.Date}</Text>
+                <Text >{prop.Description}</Text>
+            </Card>
+        )
+
+        })
+      )
+
+    } else {
+      return(
+        <Text>NONE</Text>
+      )
+    }
+               
+}
+
+const styles = StyleSheet.create({
+  header:{
+    backgroundColor:'#FF0000',
+  },
+  screenName:{
+    alignSelf:'center',
+    fontWeight:'bold',
+    fontSize:20,
+    color:'#FFFFFF',
+    marginBottom:10
+  },
+  banner:{
+    width:400,
+    marginTop:-100,
+    marginBottom:-90
+  },
+  icon:{
+    marginTop:40,
+    marginLeft:15
+  },
+  title:{
+    fontWeight:'bold',
+    fontSize:20,
+    color:'#000000'
+  },
+  date:{
+    fontWeight:'100',
+    fontSize:12,
+    marginBottom:10
+  }
+});
+
+export default MasterScreen;
