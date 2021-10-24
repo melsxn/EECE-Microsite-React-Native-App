b6d830dd65b5e1ce8adabaf7c66dfcc0e9cac671
Melson <melsonzpcanlas@gmail.com>
2021-10-25 03:11:25 +0800

    Update RootStack drawer and navigation

--- navigator/RootStack.js
+++ navigator/RootStack.js
@@ -18,50 +18,62 @@ import BachelorScreen from '../screens/BachelorScreen';
 import MasterScreen from '../screens/MasterScreen';
 import PhDScreen from '../screens/PhDScreen';
 
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-
+const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
+
 const {primary, tertiary} = Colors;
+
+
+const HomeNavigator = () => {
+  return(
+    <Stack.Navigator initialRouteName="ProgramPage">
+      <Stack.Screen name="AdminScreen" component={AdminScreen} />
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="ProgramPage" component={ProgramPage} />
+      <Stack.Screen name="BachelorScreen" component={BachelorScreen} />
+      <Stack.Screen name="MasterScreen" component={MasterScreen} />
+      <Stack.Screen name="PhDScreen" component={PhDScreen} />
+    </Stack.Navigator>
+  )
+}
+
+const AdminNavigator = () => {
+  return(
+    <Stack.Navigator initialRouteName="LoginScreen">
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="AddAnnouncement" component={AddAnnouncement} />
+      <Stack.Screen name="EditAnnouncement" component={EditAnnouncement} />
+      <Stack.Screen name="DeleteAnnouncement" component={DeleteAnnouncement} />
+      <Stack.Screen name="AddProgram" component={AddProgram} />
+      <Stack.Screen name="EditProgram" component={EditProgram} />
+      <Stack.Screen name="DeleteProgram" component={DeleteProgram} />
+      <Stack.Screen name="AdminScreen" component={AdminScreen} />
+    </Stack.Navigator>
+  )
+}
+
+
 
 const RootStack = () => {
     return(
         <NavigationContainer>
-            <Stack.Navigator 
-                screenOptions={{
-                    headerStyle:{
-                        backgroundcolor: 'transparent'
-                    },
-                    headerTintColor: primary,
-                    headerTransparent: true,
-                    headerTitle:'',
-                    headerShown: false,
-                    headerLeft: null,
-                    
-                    
-            }}
-            initialRouteName="LoginScreen"
-            >
-            <Stack.Group>
-                <Stack.Screen name="LoginScreen" component={LoginScreen} />
-                <Stack.Screen options={{headerTintColor : '#FFFFFF',}} name="AdminScreen" component={AdminScreen} />
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="ProgramPage" component={ProgramPage} />
-                <Stack.Screen name="BachelorScreen" component={BachelorScreen} />
-                <Stack.Screen name="MasterScreen" component={MasterScreen} />
-                <Stack.Screen name="PhDScreen" component={PhDScreen} />
-            </Stack.Group>
-            <Stack.Group>
-                <Stack.Screen name="Afterlog" component={Afterlog} />
-                <Stack.Screen name="AddAnnouncement" component={AddAnnouncement} />
-                <Stack.Screen name="EditAnnouncement" component={EditAnnouncement} />
-                <Stack.Screen name="DeleteAnnouncement" component={DeleteAnnouncement} />
-                <Stack.Screen name="AddProgram" component={AddProgram} />
-                <Stack.Screen name="EditProgram" component={EditProgram} />
-                <Stack.Screen name="DeleteProgram" component={DeleteProgram} />
-            </Stack.Group>
-
-
-            </Stack.Navigator>
+        <Drawer.Navigator initialRouteName="Home" screenOptions={{
+           drawerStyle: {
+             backgroundColor: '#FFFFCC'
+           },
+           headerShown: true,
+           headerStyle:{
+             backgroundColor:'red'
+           }
+        }}>
+           <Drawer.Screen name="Home" component={Home}/>
+           <Drawer.Screen name="Login" component={AdminNavigator}/>
+           <Drawer.Screen name="Programs" component={HomeNavigator}/>
+           
+        </Drawer.Navigator>
+        
         </NavigationContainer>
     )
 
